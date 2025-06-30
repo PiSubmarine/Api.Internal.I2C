d67846b917b52c905ea5846f59f93265ab6ff329
Dmitriy Monakhov <dimamon6032403@gmail.com>
2025-06-30 11:43:13 +0200

    Encapsulated in a namespace

--- public/PiSubmarine/Api/Internal/I2CDriverConcept.h
+++ public/PiSubmarine/Api/Internal/I2CDriverConcept.h
@@ -1,12 +1,17 @@
 #pragma once
 
-using I2CCallback = std::function<void(uint8_t deviceAddress, bool)>;
-
-template<typename T>
-concept I2CDriverConcept = requires(T driver, uint8_t deviceAddress, uint8_t * txData, uint8_t * rxData, size_t len, I2CCallback callback)
+namespace PiSubmarine::Api::Internal
 {
-	{ driver.Write(deviceAddress, txData, len) } -> std::same_as<bool>;
-	{ driver.Read(deviceAddress, rxData, len) } -> std::same_as<bool>;
-	{ driver.WriteAsync(deviceAddress, txData, len, callback) } -> std::same_as<bool>;
-	{ driver.ReadAsync(deviceAddress, rxData, len, callback) } -> std::same_as<bool>;
-};
+
+	using I2CCallback = std::function<void(uint8_t deviceAddress, bool)>;
+
+	template<typename T>
+	concept I2CDriverConcept = requires(T driver, uint8_t deviceAddress, uint8_t * txData, uint8_t * rxData, size_t len, I2CCallback callback)
+	{
+		{ driver.Write(deviceAddress, txData, len) } -> std::same_as<bool>;
+		{ driver.Read(deviceAddress, rxData, len) } -> std::same_as<bool>;
+		{ driver.WriteAsync(deviceAddress, txData, len, callback) } -> std::same_as<bool>;
+		{ driver.ReadAsync(deviceAddress, rxData, len, callback) } -> std::same_as<bool>;
+	};
+
+}
